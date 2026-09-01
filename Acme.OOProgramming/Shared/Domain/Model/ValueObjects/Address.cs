59f856a0b656008332397c0334946d43ba7aae40
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-01 09:18:30 -0500

    feat(address): add address value object.

---
+++ Acme.OOProgramming/Shared/Domain/Model/ValueObjects/Address.cs
@@ -0,0 +1,49 @@
+namespace Acme.OOProgramming.Shared.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents an international physical address value object.
+/// </summary>
+public record Address
+{
+    public string Street { get; init; }
+    public string Number { get; init; }
+    public string City { get; init; }
+    public string? StateOrRegion { get; init; }
+    public string PostalCode { get; init; }
+    public string Country { get; init; }
+    
+    public Address(
+        string street,
+        string number,
+        string city,
+        string? stateOrRegion,
+        string postalCode,
+        string country)
+    {
+
+        if (string.IsNullOrWhiteSpace(street))
+            throw new ArgumentException("Street cannot be null or empty", nameof(street));
+        
+        if (string.IsNullOrWhiteSpace(number))
+            throw new ArgumentException("Number cannot be null or empty", nameof(number));
+        
+        if (string.IsNullOrWhiteSpace(city))
+            throw new ArgumentException("City cannot be null or empty", nameof(city));
+        
+        if (string.IsNullOrWhiteSpace(postalCode))
+            throw new ArgumentException("Postal code cannot be null or empty", nameof(postalCode));
+        
+        if (string.IsNullOrWhiteSpace(country))
+            throw new ArgumentException("Country cannot be null or empty", nameof(country));
+        
+        Street = street;
+        Number = number;
+        City = city;
+        StateOrRegion = stateOrRegion;
+        PostalCode = postalCode;
+        Country = country;
+    }
+    
+    public override string ToString() => $"{Street}, {Number}, {City}, {StateOrRegion}, {PostalCode}, {Country}";
+    
+}
